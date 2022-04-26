d22194ef7842a463683751010614770410e70911
Adnan-Alfadhli <adnan2000611@gmail.com>
2022-04-27 00:37:04 +0300

    ..

--- Interface-classes/src/pkginterface/classes/InterfaceClasses.java
+++ Interface-classes/src/pkginterface/classes/InterfaceClasses.java
@@ -10,11 +10,11 @@ package pkginterface.classes;
  */
 public class InterfaceClasses {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
     }
     
 }
---
+++ Interface-classes/src/pkginterface/classes/Shape.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkginterface.classes;
+
+/**
+ *
+ * @author adnan
+ */
+
+public interface Shape {
+    
+     public abstract void computeArea();
+     
+}
